2f857d96ef2c27b8bdae5e1fd313b54e5979c586
jfoulad <99297166+jfoulad@users.noreply.github.com>
2023-04-14 13:15:09 -0700

    updating hours storyboard and vc

--- Planvy/Controller/HoursViewController.swift
+++ Planvy/Controller/HoursViewController.swift
@@ -8,14 +8,47 @@
 import UIKit
 
 class HoursViewController: UIViewController {
-
     
-    
+    let designManager = ColorAndFontManager.shared
     var formattedSchedule: Dictionary<String,String> = [:]
     
     
-//    test test testy test test
 
+    @IBOutlet weak var hoursLabel: UILabel!
     
     
+    @IBOutlet weak var mondayLabel: UILabel!
+    
+    @IBOutlet weak var tuesdayLabel: UILabel!
+    
+    @IBOutlet weak var wednesdayLabel: UILabel!
+    
+    
+    @IBOutlet weak var thursdayLabel: UILabel!
+    
+    @IBOutlet weak var fridayLabel: UILabel!
+    
+    @IBOutlet weak var saturdayLabel: UILabel!
+    
+    @IBOutlet weak var sundayLabel: UILabel!
+    
+    override func viewDidLoad() {
+        
+//        Set up UI colors etc
+        setUpUI()
+    }
+    
+    
+    
+    func setUpUI() {
+        hoursLabel.font = designManager.font(weight: .Bold, size: 25)
+        hoursLabel.textColor = designManager.black
+        
+        let allDayLabels = [mondayLabel, tuesdayLabel, wednesdayLabel, thursdayLabel, fridayLabel, saturdayLabel, sundayLabel]
+        
+        for label in allDayLabels {
+            label?.font = designManager.font(weight: .Medium, size: 17)
+            label?.textColor = designManager.black
+        }
+    }
 }
